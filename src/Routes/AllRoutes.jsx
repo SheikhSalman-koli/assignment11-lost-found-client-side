3e591fcdb989b2e94524864955e6b31d827f81fd
SheikhSalman <assalmanmuhammad@gmail.com>
2025-06-12 22:06:00 +0600

    first setup commit

---
+++ src/Routes/AllRoutes.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Layout/Root";
+import Home from "../Pages/Home";
+import Signin from "../Pages/Signin";
+import Signup from "../Pages/Signup";
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        Component: Root,
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+            {
+                path: 'signin',
+                Component: Signin
+            },
+              {
+                path: 'signup',
+                Component: Signup
+            }
+        ] 
+    }
+])
